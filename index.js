5327857601b834255b800dd8faf384af4a4787f6
Jhon rojas <jhon.rojaas@synergykey.it>
2020-12-06 11:10:19 +0100

    commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  max: 1000 // limit each IP to 100 requests per windowMs
 });
 
 const app = express();
