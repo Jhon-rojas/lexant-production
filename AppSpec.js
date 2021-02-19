9079503ee5c9d30e5df6e44fd0e12cb334399266
Jhon rojas <jhon.rojaas@synergykey.it>
2021-02-19 17:08:57 +0100

    commit

--- AppSpec.js
+++ AppSpec.js
@@ -2,35 +2,70 @@ const fs = require("fs");
 const convert = require("xml-js");
 const https = require("https");
 
+let okhttp = {}
+var itemsProcessed = 0;
+
 const xmlFile = fs.readFileSync(__dirname + "/dist/lexant/sitemap.xml", "utf8");
 
 const jsonData = JSON.parse(
-  convert.xml2json(xmlFile, { compact: true, spaces: 2 })
+    convert.xml2json(xmlFile, {
+        compact: true,
+        spaces: 2
+    })
 );
 
 MultiHttp(jsonData.urlset.url)
 
+
+
 function MultiHttp(paths) {
 
-  paths.forEach((element) => {
 
-    let options = {
-      host: element.loc._text.replace("https://", "").split('/')[0],
-      path: element.loc._text.replace("https://", "").split('/').slice(1).join("/"),
-      method: "GET",
-    };
-    // console.log(element.loc._text.replace("https://", "").split('/')[0])
-    // console.log("/"+element.loc._text.replace("https://", "").split('/').slice(1).join("/"))
-    const req = https.request(options, (res) => {
-        console.log(res.statusCode)
+    paths.forEach((element) => {
+
+
+        if (req(element).ok === true) {
+            okhttp[req(element).path] = "Success"
+        } else {
+            okhttp[req(element).path] = req(element).ok
+        }
+
+
+        itemsProcessed++;
+
+        if (itemsProcessed === paths.length) {
+            console.table(okhttp)
+        }
 
     });
 
-    req.on("error", (error) => {
-        console.log(error)
-    });
 
-    req.end();
-  });
+}
 
+function req(element) {
+
+    try {
+        let options = {
+            host: element.loc._text.replace("https://", "").split('/')[0],
+            path: "/" + element.loc._text.replace("https://", "").split('/').slice(1).join("/"),
+            method: "GET",
+        };
+
+
+        const req = https.request(options)
+
+
+        req.end();
+
+        return {
+            path: options.host + options.path,
+            ok: true
+        }
+
+    } catch (e) {
+        return {
+            path: element,
+            ok: e
+        }
+    }
 }
